0b8c37f94f9c3cff7218d9fc9b420182860c5413
Fulup Ar Foll <fulup@iot.bzh>
2021-01-07 16:11:53 +0100

    remove build from git

--- build/conf.d/project/etc/CMakeFiles/spawn-config.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/spawn-config"
-  "spawn-config"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/spawn-config.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
