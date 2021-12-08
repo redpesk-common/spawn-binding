ec8ae87915dc3300bfde2d8f9e69ae6b8eff6ea3
Ronan Le Martret <ronan.lemartret@iot.bzh>
2021-12-08 10:02:08 +0100

    Remove rpath to fix build
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- conf.d/cmake/00-opensuse-config.cmake
+++ conf.d/cmake/00-opensuse-config.cmake
@@ -6,8 +6,5 @@ list(APPEND PKG_REQUIRED_LIST uthash)
 # Libshell is not part of standard Linux Distro (https://libshell.org/)
 set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:/usr/local/lib64/pkgconfig")
 
-# Remember sharelib path to simplify test & debug
-set(BINDINGS_LINK_FLAG "-Xlinker -rpath=/usr/local/lib64")
-
 # memfd_create not present even on OpenSuse-15.2
 add_definitions(-DMEMFD_CREATE_MISSING)
--- conf.d/cmake/01-ubuntu-config.cmake
+++ conf.d/cmake/01-ubuntu-config.cmake
@@ -4,6 +4,3 @@ list(APPEND PKG_REQUIRED_LIST lua53-c++>=5.3)
 
 # Libshell is not part of standard Linux Distro (https://libshell.org/)
 set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:/usr/local/lib64/pkgconfig")
-
-# Remeber sharelib path to simplify test & debug
-set(BINDINGS_LINK_FLAG "-Xlinker -rpath=/usr/local/lib64")
